44728bca2ad8c43628dc648e2df4ebbcae6b142b
sennescheepers <senne.scheepers@gmail.com>
2021-03-13 16:01:48 +0100

    initial commit

--- js/locale.js
+++ js/locale.js
@@ -2,22 +2,31 @@
 
 const setEnglish = () => {
     console.log('setting language to english');
+    document.getElementById("location").innerHTML = "Limburg, Belgium";
+
 };
 
 const setDutch = () => {
     console.log('tekst in het nederlands zetten');
+
+    document.getElementById("location").innerHTML = "Limburg, België";
 };
 
 const setFrench = () => {
     console.log('traduire en francais');
+
+    document.getElementById("location").innerHTML = "Limbourg, Belgique";
 };
 
 const setGerman = () => {
     console.log('umsetzen auf Deutsch');
+
+    document.getElementById("location").innerHTML = "Limburg, Belgien";
 };
 
 const handleLanguage = (e) => {
     let selectedLanguage = document.getElementById('language-picker-select').value;
+    console.log(selectedLanguage)
     switch (selectedLanguage) {
         case 'english':
             setEnglish();
